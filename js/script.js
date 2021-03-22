0d07558df99c5fe6871389dec398c2fd9ed5faaf
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-03-22 13:58:08 -0400

    Created an array with 5 pokemon and their traits

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
+let pokemonList = [
+  {
+    name: "Charizard",
+    height: "1.7m",
+    type: ["fire","flying"]
+  },
 
+  {
+    name: "Pikachu",
+    height:"0.4m",
+    type: "electric"
+  },
+
+  {
+    name: "Rhydon",
+    height: "1.9m",
+    type: ["rock","ground"]
+  },
+
+  {
+    name: "Squirtle",
+    height: "0.5m",
+    type: "water"
+  },
+
+  {
+    name: "Zapdos",
+    height: "1.6m",
+    type: ["elecric","flying"]
+  }
+];
+
+console.log(pokemonList);
