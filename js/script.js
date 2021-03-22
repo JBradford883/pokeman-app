64809431201c717373f93400290c87e95b858d56
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-03-22 14:11:24 -0400

    Removed m from height

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 let pokemonList = [
   {
     name: "Charizard",
-    height: "1.7m",
+    height: 1.7,
     type: ["fire","flying"]
   },
 
   {
     name: "Pikachu",
-    height:"0.4m",
+    height: 0.4,
     type: "electric"
   },
 
   {
     name: "Rhydon",
-    height: "1.9m",
+    height: 1.9,
     type: ["rock","ground"]
   },
 
   {
     name: "Squirtle",
-    height: "0.5m",
+    height: 0.5,
     type: "water"
   },
 
   {
     name: "Zapdos",
-    height: "1.6m",
+    height: 1.6,
     type: ["elecric","flying"]
   }
 ];
