54030fcf8454c062176f7f95568f870dbb3519e4
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-03-30 08:49:45 -0400

    Cleaned up code, aligned curly brackets for better visibility

--- js/script.js
+++ js/script.js
@@ -1,60 +1,62 @@
 //Array of Pokemon Name,Height,Type
+
 let pokemonRepository = (function () {
-let pokemonList = [
-  {
-    name: "Charizard",
-    height: 1.7,
-    type: ["fire","flying"]
-  },
+  let pokemonList = [
+    {
+      name: "Charizard",
+      height: 1.7,
+      type: ["fire","flying"]
+    },
 
-  {
-    name: "Pikachu",
-    height: 0.4,
-    type: ["electric"]
-  },
+    {
+      name: "Pikachu",
+      height: 0.4,
+      type: ["electric"],
+    },
 
-  {
-    name: "Rhydon",
-    height: 1.9,
-    type: ["rock","ground"]
-  },
+    {
+      name: "Rhydon",
+      height: 1.9,
+      type: ["rock","ground"]
+    },
 
-  {
-    name: "Squirtle",
-    height: 0.5,
-    type: ["water"]
-  },
+    {
+      name: "Squirtle",
+      height: 0.5,
+      type: ["water"],
+    },
 
-  {
-    name: "Zapdos",
-    height: 1.6,
-    type: ["electric","flying"]
-  }
-];
+    {
+      name: "Zapdos",
+      height: 1.6,
+      type: ["electric","flying"]
+    }
+  ];
 
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
+function getAll() {
+  return pokemonList;
+}
 
-  function getAll() {
-    return pokemonList;
-  }
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
 
-  return {
-    add: add,
-    getAll: getAll
-  };
+return {
+  add: add,
+  getAll: getAll
+}
+
 })();
 
-
 // Added forEach loop for Pokemon List
-function pokemonDescription(pokemonList) {
-  if (pokemon.height > 1.8){
-  document.write(pokemon.name + ' - ' + pokemon.height + ' m - Wow! That is a big Pokemon! <br>');
-}else {document.write(pokemon.name + ' - ' + pokemon.height + ' m <br>')
-}
+function pokemonDescription(pokemon) {
+  if (pokemon.height > 1.8) {
+    document.write(pokemon.name + ' - ' + pokemon.height + 'm - Wow! That is a big Pokemon! <br>');
+  }else {
+    document.write(pokemon.name + ' - ' + pokemon.height + 'm <br>')
+  }
 }
 
 pokemonList.forEach(pokemonDescription);
-pokemonRepository.add({ name: 'Wartorle', height: 1, type: 'water' });
+pokemonRepository.add( {name: 'Wartorle', height: 1, type: 'water'})
 document.write(pokemonRepository.getAll());
