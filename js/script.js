0be0ff714f322e9b16a193ea377cfef7d82e697a
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-04-01 14:28:48 -0400

    Added load detail function to call details from api

--- js/script.js
+++ js/script.js
@@ -54,12 +54,26 @@ function addListItem(pokemon){
   }
 
 }
+  //Loads List Details
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
 
 return {
   add: add,
   getAll: getAll,
   addListItem: addListItem
 }
+      //Add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
 
 })();
 
