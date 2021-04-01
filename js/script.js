9279947ca02c973de7b691dc1ee6ff224d6b041e
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-04-01 14:25:37 -0400

    Removed internal list to call to outside api

--- js/script.js
+++ js/script.js
@@ -1,34 +1,5 @@
 //IIFE with Pokemon Repository
 let pokemonRepository = (function createPokemonRepository () {
-  let repository = [
-    {
-      name: "Charizard",
-      height: 1.7,
-      type: ["fire","flying"]
-    },
-
-    {
-      name: "Pikachu",
-      height: 0.4,
-      type: ["electric"],
-    },
-
-    {
-      name: "Rhydon",
-      height: 1.9,
-      type: ["rock","ground"]
-    },
-
-    {
-      name: "Squirtle",
-      height: 0.5,
-      type: ["water"],
-    },
-
-    {
-      name: "Zapdos",
-      height: 1.6,
-      type: ["electric","flying"]
     }
   ];
 
