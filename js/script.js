7f56d0171d58eb699aa65866f80b3f38744f8f84
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-04-05 22:43:50 -0400

    Updated note for pokemon details

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ let pokemonRepository = (function createPokemonRepository () {
       return response.json();
     }).then(function (details) {
 
-      //Add the details to the item
+      //Add Details to the item
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.types = details.types;
